108ff16ff0ade57cdf64085afa5ebeb67c9236c8
dlwlgusdev <plus3456789@naver.com>
2024-11-01 11:13:27 +0900

    feat : 마이페이지 UI 생성

---
+++ src/routes/ops/mypage/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { Tabs, TabItem } from 'flowbite-svelte';
+	import { user } from '../../../stores/user';
+	import { onMount } from 'svelte';
+	import { isUser, type User } from '$lib/types/User';
+
+	export let userData: User;
+
+	onMount(() => {
+		$user = userData;
+	});
+</script>
+
+{#if isUser($user)}
+	<div>
+		<Tabs>
+			<TabItem open title="개인 정보">
+				<p class="text-sm text-gray-500 dark:text-gray-400">
+					<b>개인 정보</b>
+					성함 : {$user.name}
+					이메일 : {$user.email}
+				</p>
+			</TabItem>
+			<TabItem title="배송지 정보">
+				<p class="text-sm text-gray-500 dark:text-gray-400">
+					<b>Settings:</b>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+					labore et dolore magna aliqua.
+				</p>
+			</TabItem>
+			<TabItem title="개인 정보 수정">
+				<p class="text-sm text-gray-500 dark:text-gray-400">
+					<b>Users:</b>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+					labore et dolore magna aliqua.
+				</p>
+			</TabItem>
+		</Tabs>
+	</div>
+{:else}
+	<p>고객님의 정보가 아직 로드되지 않았습니다.</p>
+{/if}
