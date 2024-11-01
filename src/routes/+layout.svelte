dedabc230f5d42a01bf355f127b2ac69f3a0e1bc
dlwlgusdev <plus3456789@naver.com>
2024-11-01 11:09:55 +0900

    feat : 메인페이지 생성

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { onDestroy, onMount } from 'svelte';
+	import { loadBookList } from '$lib/loadBookList';
+	import { bookList } from '../stores/bookList';
+
+	import '../app.css';
+	import { loadUserList } from '$lib/loadUserList';
+	import { userList } from '../stores/userList';
+
+	onMount(async () => {
+		// TODO: 페이지 번호 받아오는 부분 추가하거나, 필요 하에 "이 데이터 로드 로직"을 이동시키기
+		const _bookList = await loadBookList(0);
+
+		bookList.set(_bookList);
+	});
+
+	onDestroy(() => {
+		bookList.set(undefined);
+		userList.set(undefined);
+	});
+</script>
+
+<div class="h-screen">
+	<slot></slot>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import MainHeader from '../components/MainHeader.svelte';
+	import BookCover from '../components/BookCover.svelte';
+</script>
+
+<MainHeader></MainHeader>
+<h1>추천책!</h1>
+<BookCover imgsrc="/Book1.png" />
