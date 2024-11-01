fa523b9249769ac9aa2c51cad15ba18843194e23
dlwlgusdev <plus3456789@naver.com>
2024-11-01 11:13:13 +0900

    feat : 장바구니 ui 생성

---
+++ src/routes/ops/cart/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { cart } from '../../../stores/cart';
+</script>
+
+<div>
+	<ul>
+		{#each $cart as { id, name, price, genre, author, publishedDate, coverUrl }, i}
+			<li>
+				<h1>{i + 1} : {name} {price}</h1>
+			</li>
+		{/each}
+	</ul>
+</div>
