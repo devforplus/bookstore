f82c86e6a4b7e2458835b0bdbef078fd831b29ce
dlwlgusdev <plus3456789@naver.com>
2024-11-04 02:10:39 +0900

    feat : Header 코드 완성

--- src/components/MainHeader.svelte
+++ src/components/MainHeader.svelte
@@ -1,101 +1,25 @@
 <script lang="ts">
-	import IconStore from './IconStore.svelte';
 	import Login from './login.svelte';
-	import MypageIcon from './MypageIcon.svelte';
 	import Register from './register.svelte';
 	import SearchBar from './SearchBar.svelte';
-	import Title from './Title.svelte';
+	import Brand from './Brand.svelte';
+
+	import Icon from '@iconify/svelte';
+	import { CartOutline } from 'flowbite-svelte-icons';
 </script>
 
-<header class="header">
-	<div class="logo">
-		<Title></Title>
-	</div>
-	<div class="search-container">
+<!-- <header class="relative flex items-center justify-between border-b border-gray-200 bg-white p-8"> -->
+<header class="flex flex-row place-items-center justify-between gap-4 p-8">
+	<Brand></Brand>
+
+	<div class="w-3/5">
 		<SearchBar></SearchBar>
 	</div>
-	<div class="user-actions">
-		<div class="auth-links">
-			<div class="signup">
-				<Register></Register>
-			</div>
-			<div class="login">
-				<Login></Login>
-			</div>
-		</div>
-		<div class="icon-links">
-			<div class="cart-icon">
-				<IconStore></IconStore>
-			</div>
-			<div class="user-icon">
-				<MypageIcon></MypageIcon>
-			</div>
-		</div>
+
+	<div class="flex place-items-center gap-4">
+		<Register></Register>
+		<Login></Login>
+		<CartOutline size="xl"></CartOutline>
+		<Icon icon="iconamoon:profile" font-size="xx-large" />
 	</div>
 </header>
-
-<style>
-	.header {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		padding: 30px;
-		background-color: #fff;
-		border-bottom: 1px solid #eaeaea;
-		position: relative;
-	}
-
-	.logo {
-		display: flex;
-		align-items: center;
-	}
-
-	.user-actions {
-		display: flex;
-		flex-direction: column; /* 세로 방향으로 배치 */
-		align-items: flex-end; /* 오른쪽 정렬 */
-		position: absolute;
-		right: 16px;
-		top: 6px;
-	}
-
-	.auth-links {
-		display: flex;
-		align-items: center;
-	}
-
-	.signup,
-	.login {
-		margin-left: 16px;
-		text-decoration: none;
-		color: #333;
-	}
-
-	.signup:hover,
-	.login:hover {
-		text-decoration: underline;
-	}
-
-	.icon-links {
-		display: flex;
-		align-items: center;
-		margin-top: 8px; /* 간격 조정 */
-	}
-
-	.user-icon {
-		border-radius: 50%;
-		padding: 2px;
-	}
-	.cart-icon {
-		padding: 4px;
-	}
-	.search-container {
-		display: flexbox;
-		align-items: center;
-		position: absolute; /* 절대 위치 설정 */
-		left: 50%; /* 중앙 정렬을 위한 설정 */
-		transform: translateX(-50%); /* 중앙으로 이동 */
-		margin-top: 10px; /* 사용자 액션과의 간격 조정 */
-		width: 70%;
-	}
-</style>
