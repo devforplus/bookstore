0e512031c64d9a82f9bd856eb79a65382eeb1459
dlwlgusdev <plus3456789@naver.com>
2024-11-04 02:09:22 +0900

    feat : 책 리스트가 페이지 번호 상태에 맞게 갱신되도록 수정

--- src/stores/bookList.ts
+++ src/stores/bookList.ts
@@ -1,6 +1,9 @@
 import { loadBookList } from "$lib/loadBookList";
-import type { Book } from "$lib/types/Book";
 import { writable } from "svelte/store";
+
+import type { Book } from "$lib/types/Book";
+import { currentPage } from "./currentPage";
+import { onDestroy } from "svelte";
 
 /**
  * ## 책 리스트 상태
@@ -8,4 +11,21 @@ import { writable } from "svelte/store";
  * - 데이터가 로드되지 않았을 때는 `undefined`, 로드된 이후에는 `Book[]` 타입을 따릅니다.
  * - 데이터는 {@link loadBookList} 함수를 통해 로드할 수 있습니다.
  */
-export const bookList = writable<Book[] | undefined>(undefined);
+export const bookList = (() => {
+	const $bookList = writable<Book[] | undefined>(undefined);
+
+	const unsubscribeCurrentPage = currentPage.subscribe(async ($currentPage) => {
+		try {
+			const bookData = await loadBookList($currentPage);
+			$bookList.set(bookData);
+		} catch (err) {
+			$bookList.set(undefined);
+		}
+	});
+
+	onDestroy(() => {
+		unsubscribeCurrentPage();
+	});
+
+	return $bookList;
+})();
