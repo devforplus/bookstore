c879deec2c7d0f0b4e019015c2b72992c56c1fef
dlwlgusdev <plus3456789@naver.com>
2024-11-04 02:10:10 +0900

    refactor : 불필요한 호출 제거

--- src/stores/purchase.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store";
-
-import type { Book } from "$lib/types/Book";
-
-export const purchase = writable<Book | undefined>(undefined);
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,5 +1,4 @@
-import { loadUserList } from "$lib/loadUserList";
-import { UserSchema, type User } from "$lib/types/User";
+import type { User } from "$lib/types/User";
 import { writable } from "svelte/store";
 
 export const user = writable<User | undefined>(undefined);
