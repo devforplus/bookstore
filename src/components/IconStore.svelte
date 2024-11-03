8aa4a518d0a71ce0590a76deca3714098d84b9f4
dlwlgusdev <plus3456789@naver.com>
2024-11-04 03:44:49 +0900

    remove : 장바구니 아이콘, 마이페이지 아이콘 제거
    이 내용은 별도의 라이브러리를 통해 사용하기 시작함

--- src/components/IconStore.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<div>
-	<a href="/ops/cart"><img alt="장바구니 아이콘" width="40" src="/StoreIcon.png" /></a>
-</div>
--- src/components/MypageIcon.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<div>
-	<a href="/ops/mypage"><img src="/Mypage.jpg" width="40" alt="마이페이지 아이콘" /></a>
-</div>
