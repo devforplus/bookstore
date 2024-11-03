fad3a46784da5e64b5972b6b9393fce9a31ac57b
dlwlgusdev <plus3456789@naver.com>
2024-11-04 02:09:07 +0900

    feat : 장바구니 상태에 영속성 추가

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,17 +1,17 @@
-import { writable } from 'svelte/store';
+import { sessionWritable } from "@macfja/svelte-persistent-store";
 
-import type { Book } from '$lib/types/Book';
+import type { Book } from "$lib/types/Book";
 
 /**
  * ### 장바구니 상태
- * 
+ *
  * - `"new book data"`는 새로운 책 데이터를 의미함. `Book` 인터페이스에 맞게 작성하면 됨
- * 
+ *
  * @example
  * // In Svelte Component
  * $cart = [...$cart, {"new book data"}];
- * 
+ *
  * // In TypeScript
  * cart.update(prevCartList => [...prevCartList, "new book data"]);
  */
-export const cart = writable<Book[]>([]);
+export const cart = sessionWritable<Book[]>("cart", []);
