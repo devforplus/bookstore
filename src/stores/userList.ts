fa98c94b92ee23a8dbc94dfcaec24aafddba442b
dlwlgusdev <plus3456789@naver.com>
2024-11-04 03:45:16 +0900

    remove : 사용자 리스트 상태 제거 (보안 관련)

--- src/stores/userList.ts
+++
@@ -1,5 +0,0 @@
-import { loadUserList } from "$lib/loadUserList";
-import { UserSchema, type User } from "$lib/types/User";
-import { writable } from "svelte/store";
-
-export const userList = writable<User[] | undefined>(undefined);
