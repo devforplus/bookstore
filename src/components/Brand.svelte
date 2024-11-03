10fbb372756ed450e16ce2f369b1412c750f46bf
dlwlgusdev <plus3456789@naver.com>
2024-11-04 02:11:02 +0900

    feat : Title -> Brand 이름 변경 및 코드개선

---
+++ src/components/Brand.svelte
@@ -0,0 +1,1 @@
+<a href="/" class="text-3xl font-bold text-blue-900">동서 서점</a>
--- src/components/Title.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { goto } from '$app/navigation';
-</script>
-
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<!-- svelte-ignore a11y-no-static-element-interactions -->
-<span on:click={() => goto('/')}>동서 서점</span>
