e877a3f19cbe21051a32a8ecd60021621ef0f5a4
devcomfort <kimdonghyun026@gmail.com>
2024-11-04 13:11:07 +0000

    feat : 책 최신순 정렬하여 보여주는 기능 추가

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
+	import { isArray, range } from 'remeda';
+	import { Skeleton } from 'flowbite-svelte';
+
+	import BookInfo from '../components/BookInfo.svelte';
 	import MainHeader from '../components/MainHeader.svelte';
-	import BookCover from '../components/BookCover.svelte';
+	import { bookList } from '../stores/bookList';
+	import type { Book } from '$lib/types/Book';
+
+	/** 책 리스트를 최신순으로 정렬 */
+	let recentBooks: Book[] | undefined = undefined;
+
+	$: {
+		recentBooks = $bookList
+			// 시간 기준 오름차순 정렬
+			?.toSorted(
+				(bookA, bookB) =>
+					new Date(bookA.publishedDate).getTime() - new Date(bookB.publishedDate).getTime()
+			)
+			// 내림차순으로 변경
+			.toReversed();
+	}
 </script>
 
 <MainHeader></MainHeader>
-<!-- TODO: 추천도서 기능 추가? -->
+
+<span class="text-2xl font-bold text-blue-900">최신 도서</span>
+<div class="flex flex-col gap-1">
+	{#if isArray(recentBooks)}
+		{#each recentBooks.slice(0, 5) as book}
+			<BookInfo {book}></BookInfo>
+		{/each}
+	{:else}
+		{#each range(0, 10) as _}
+			<Skeleton size="xxl" class="mb-2.5 mt-8" />
+		{/each}
+	{/if}
+</div>
