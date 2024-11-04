07945f9b7c8501a1af67594a7b09392eeefa4d30
devcomfort <kimdonghyun026@gmail.com>
2024-11-04 12:25:06 +0000

    feat : writable -> readable로 변경

--- frontend/src/stores/bookList.ts
+++ frontend/src/stores/bookList.ts
@@ -1,5 +1,5 @@
 import { loadBookList } from "$lib/loadBookList";
-import { writable } from "svelte/store";
+import { writable, type Readable } from "svelte/store";
 
 import type { Book } from "$lib/types/Book";
 import { currentPage } from "./currentPage";
@@ -10,8 +10,11 @@ import { currentPage } from "./currentPage";
  * - 데이터가 로드되지 않았을 때는 `undefined`, 로드된 이후에는 `Book[]` 타입을 따릅니다.
  * - 데이터는 {@link loadBookList} 함수를 통해 로드할 수 있습니다.
  */
-export const bookList = (() => {
+export const bookList: Readable<Book[] | undefined> = (() => {
+	// 상태 생성
 	const $bookList = writable<Book[] | undefined>(undefined);
+
+	// 책 데이터 로드 (csv, 임시)
 
 	currentPage.subscribe(async ($currentPage) => {
 		try {
@@ -22,5 +25,7 @@ export const bookList = (() => {
 		}
 	});
 
-	return $bookList;
+	return {
+		subscribe: $bookList.subscribe,
+	};
 })();
