4c440accf6929e765d4cc5649aeeba3051118ac8
devcomfort <kimdonghyun026@gmail.com>
2024-11-04 13:56:07 +0000

    feat ; 검색 결과가 출력되도록 코드 변경

--- frontend/src/routes/store/search/+page.svelte
+++ frontend/src/routes/store/search/+page.svelte
@@ -2,18 +2,25 @@
 
 <script lang="ts">
 	import BookInfo from '../../../components/BookInfo.svelte';
-	import { bookList } from '../../../stores/bookList';
+	import { searchResult } from '../../../stores/searchResult';
+
+	import { isEmpty } from 'remeda';
+
+	let currentPage: number = 0;
 </script>
 
-<!-- TODO: 검토 및 기능 추가 필요 -->
-{#if $bookList === undefined}
+{#if $searchResult === undefined}
 	<p>책 데이터가 아직 로드되지 않았습니다</p>
+{:else if isEmpty($searchResult)}
+	<p>검색 결과가 <strong>0</strong>건 입니다.</p>
 {:else}
 	<div>
 		<div>
-			{#each $bookList as bookData}
+			{#each $searchResult as bookData}
 				<BookInfo book={bookData} />
 			{/each}
 		</div>
 	</div>
 {/if}
+
+<!-- TODO: 페이지네이션 추가 (시간 있으면) -->
