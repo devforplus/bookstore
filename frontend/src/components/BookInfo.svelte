f852ad042909b62e3007a06caf06ca64f43eac2a
devcomfort <kimdonghyun026@gmail.com>
2024-11-04 14:14:19 +0000

    design : 레이아웃 수정

--- frontend/src/components/BookInfo.svelte
+++ frontend/src/components/BookInfo.svelte
@@ -2,14 +2,16 @@
 	import ButtonStore from './ButtonStore.svelte';
 
 	import type { Book } from '$lib/types/Book';
+	import { Input, Label } from 'flowbite-svelte';
 
-	export let book: Book;
+	// TODO: 여기도 수정
+	export let book: Book & { quantity: number };
 	/**
 	 * ### 장바구니 버튼 숨김 여부
 	 *
 	 * NOTE: 장바구니 페이지 내에서 장바구니에 상품을 추가하는 기능을 보일 필요가 없음
 	 */
-	export let hideEnCart: boolean = false;
+	export let hideEnCart = false;
 </script>
 
 <!-- 
@@ -17,7 +19,9 @@
 	제품 수량을 0 이하로 내리면 제거되도록 만드는 것도 가능함
  -->
 
-<div class="flex items-start gap-4 rounded-lg border border-gray-300 bg-white p-5 shadow-md">
+<div
+	class="flex flex-row place-items-center justify-center gap-4 rounded-lg border border-gray-300 bg-white p-5 shadow-md"
+>
 	<div class="mr-4 h-auto w-24">
 		<img src={book.coverUrl} alt={book.name} />
 	</div>
@@ -29,11 +33,18 @@
 		<p class="my-1 text-gray-600">{Intl.NumberFormat('ko-KR').format(book.price)}원</p>
 	</div>
 
-	{#if !hideEnCart}
-		<div class="mt-4">
-			<div class="text-gray-800">
-				<ButtonStore bookData={book} />
+	<div class="flex flex-col items-center gap-4">
+		{#if !hideEnCart}
+			<!-- 장바구니 숨기지 않음 -->
+			<div class="mt-4">
+				<div class="text-gray-800">
+					<ButtonStore bookData={book} />
+				</div>
 			</div>
-		</div>
-	{/if}
+		{:else}
+			<!-- 장바구니 숨김 -->
+			<Label for="quantity">수량</Label>
+			<Input bind:value={book.quantity} type="number" name="quantity" />
+		{/if}
+	</div>
 </div>
