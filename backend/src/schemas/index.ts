9e9909edf9ee31e005d49fe8db54151cf202cacc
devcomfort <kimdonghyun026@gmail.com>
2024-11-04 06:27:34 +0000

    chore : drizzle -> prisma 전환

--- backend/src/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-export { addresses } from "./addresses";
-export { books } from "./books";
-export { carts } from "./carts";
-export { genres } from "./genres";
-export { order_details } from "./order_details";
-export { orders } from "./orders";
-export { users } from "./users";
