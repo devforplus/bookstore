bc7919322eabf97fbf48fc173498efa51ed7034d
devcomfort <kimdonghyun026@gmail.com>
2024-11-04 13:42:08 +0000

    feat : 장바구니 버튼 숨김 기능 추가

--- frontend/src/components/BookInfo.svelte
+++ frontend/src/components/BookInfo.svelte
@@ -4,6 +4,12 @@
 	import type { Book } from '$lib/types/Book';
 
 	export let book: Book;
+	/**
+	 * ### 장바구니 버튼 숨김 여부
+	 *
+	 * NOTE: 장바구니 페이지 내에서 장바구니에 상품을 추가하는 기능을 보일 필요가 없음
+	 */
+	export let hideEnCart: boolean = false;
 </script>
 
 <div class="flex items-start gap-4 rounded-lg border border-gray-300 bg-white p-5 shadow-md">
@@ -18,9 +24,11 @@
 		<p class="my-1 text-gray-600">{Intl.NumberFormat('ko-KR').format(book.price)}원</p>
 	</div>
 
-	<div class="mt-4">
-		<div class="text-gray-800">
-			<ButtonStore bookData={book} />
+	{#if !hideEnCart}
+		<div class="mt-4">
+			<div class="text-gray-800">
+				<ButtonStore bookData={book} />
+			</div>
 		</div>
-	</div>
+	{/if}
 </div>
