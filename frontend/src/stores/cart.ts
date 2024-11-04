97071d2cf5eab34f5da6b0c59a95fe1505f2e3f0
devcomfort <kimdonghyun026@gmail.com>
2024-11-04 14:14:58 +0000

    feat : 장바구니 상태에 quantity 요소 임시 추가

--- frontend/src/stores/cart.ts
+++ frontend/src/stores/cart.ts
@@ -3,7 +3,7 @@ import { sessionWritable } from "@macfja/svelte-persistent-store";
 import type { Book } from "$lib/types/Book";
 import { derived } from "svelte/store";
 
-// TODO: 장바구니에 '갯수' 데이터 추가하기
+// TODO: 장바구니에 '갯수' 데이터 추가하기 (지금은 야매로, 나중에 시간있을 때 정식으로 추가하기)
 /**
  * ### 장바구니 상태
  *
@@ -16,11 +16,11 @@ import { derived } from "svelte/store";
  * // In TypeScript
  * cart.update(prevCartList => [...prevCartList, "new book data"]);
  */
-export const cart = sessionWritable<Book[]>("cart", []);
+export const cart = sessionWritable<(Book & { quantity: number })[]>("cart", []);
 
 /**
  * 장바구니에 담긴 책들의 총액
  */
 export const totalPrice = derived(cart, $cart => {
-  return $cart.reduce((totalPrice, book) => totalPrice + book.price,0)
+  return $cart.reduce((totalPrice, book) => totalPrice + book.price, 0)
 })
