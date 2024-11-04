cc3864cb70179337090ba061b29801e0d00cf2e7
devcomfort <kimdonghyun026@gmail.com>
2024-11-04 14:14:38 +0000

    feat : 결제 페이지 추가

--- frontend/src/routes/store/purchase/+page.svelte
+++ frontend/src/routes/store/purchase/+page.svelte
@@ -1,21 +1,38 @@
 <script lang="ts">
-	// TODO: 결제 페이지인 것 같음, 기능 추가하기
-	// 기존에는 바로 구매 버튼을 통해 이동했던 것 같음
+	import { Button, Card } from 'flowbite-svelte';
+	import BookInfo from '../../../components/BookInfo.svelte';
 
-	import { cart } from '../../../stores/cart';
-
-	/**
-	 * 책 총 가격
-	 * - `undefined`: "알 수 없음", "아직 책 총 가격이 계산되지 않음"
-	 */
-	let totalPrice: number | undefined = undefined;
-
-	// 책 총 가격 업데이트
-	$: totalPrice = $cart.reduce((totalPrice, book) => totalPrice + book.price, 0);
-
-	// Todo : 장바구니에 있는 데이터를 purchase로 옮겨서 총 금액 출력 및 구매하기 기능 추가
+	import { cart, totalPrice } from '../../../stores/cart';
 </script>
 
-<div>
-	현재 장바구니에 들어간 책들의 총 가격: {Intl.NumberFormat('ko-KR').format(totalPrice)}
+<div class="flex flex-col gap-2">
+	<span class="text-2xl font-bold text-blue-900">결제하기</span>
+
+	<div class="grid grid-cols-[8fr,2fr] gap-4">
+		<div class="flex flex-col gap-1">
+			{#each $cart as book}
+				<BookInfo {book} hideEnCart={true}></BookInfo>
+			{/each}
+		</div>
+
+		<div>
+			<Card class="flex flex-col gap-2">
+				<ul>
+					<li>총 도서 수: {$cart.length}</li>
+					{#each $cart as book}
+						<li>{book.name} : {Intl.NumberFormat('ko-KR').format(book.price)}원</li>
+					{/each}
+				</ul>
+
+				<hr />
+
+				<ul>
+					<li>상품 금액: {Intl.NumberFormat('ko-KR').format($totalPrice)}원</li>
+				</ul>
+
+				<!-- TODO: 결제 수단 컴포넌트 추가하기 (결제수단 안 고르면 결제 안 되게 검사 로직 추가해야 함!) -->
+				<Button on:click={() => window.alert('결제가 완료되었습니다!')}>결제하기</Button>
+			</Card>
+		</div>
+	</div>
 </div>
