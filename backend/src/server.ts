67cbd9f6c427b9511d241ba462066f030ac82a2c
devcomfort <kimdonghyun026@gmail.com>
2024-11-10 23:06:43 +0000

    feat : Express + trpc 셋업

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import * as trpcExpress from "@trpc/server/adapters/express";
+import express from "express";
+import { integratedRouter } from "src/connectors/trpc/routers";
+
+// NOTE: 문제가 생기면 아래 링크를 참고해주세요
+// https://trpc.io/docs/server/adapters/express
+
+const app = express();
+
+app.use(
+	"/trpc",
+	trpcExpress.createExpressMiddleware({
+		router: integratedRouter,
+	}),
+);
+
+app.listen(4000);
