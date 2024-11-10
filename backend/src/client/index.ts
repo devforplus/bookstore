2f393fb738e89e33a4e42b5dc589fd8fd6fa3f13
devcomfort <kimdonghyun026@gmail.com>
2024-11-10 23:10:18 +0000

    feat : Add tRPC Client Module

---
+++ backend/src/client/index.ts
@@ -0,0 +1,12 @@
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+
+/**
+ * TRPC Client 모듈
+ */
+export const client = createTRPCClient({
+	links: [
+		httpBatchLink({
+			url: "http://localhost:4000/trpc",
+		}),
+	],
+});
