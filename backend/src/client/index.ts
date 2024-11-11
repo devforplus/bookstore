e688c896bf5f1ba859bfbf921c6071c690712db0
devcomfort <kimdonghyun026@gmail.com>
2024-11-11 02:43:29 +0000

    feat : 타입 추가

--- backend/src/client/index.ts
+++ backend/src/client/index.ts
@@ -1,12 +1,13 @@
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import type { IntegratedRouter } from "../connectors/trpc/routers/integratedRouter";
 
 /**
  * TRPC Client 모듈
  */
-export const client = createTRPCClient({
+export const client = createTRPCClient<IntegratedRouter>({
 	links: [
 		httpBatchLink({
-			url: "http://localhost:4000/api",
+			url: "http://192.168.0.22:4000/api",
 		}),
 	],
 });
