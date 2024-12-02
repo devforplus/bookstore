6db23c0528c44a98be66737be4cc708eed8b01f5
devcomfort <kimdonghyun026@gmail.com>
2024-12-02 02:22:54 +0000

    fix : 불필요한 상태들 제거

--- frontend/src/routes/store/mypage/+page.svelte
+++ frontend/src/routes/store/mypage/+page.svelte
@@ -1,29 +1,9 @@
 <!-- TODO: 추후 추가 (현재 레거시한 상태) -->
 
 <script lang="ts">
-	import { Tabs, TabItem } from 'flowbite-svelte';
-	import { user } from '../../../stores/user';
-	import { onMount } from 'svelte';
-	import { isUser, type User } from '$lib/types/User';
-	import { loadUserList } from '$lib/loadUserList';
-
-	// TODO: userList 제거된 것 반영하고, 기능 추가하기
-
-	let currentUser: User | undefined;
-
-	// onMount에서 사용자 데이터 로드
-	onMount(async () => {
-		const userList = await loadUserList(0); // 사용자 목록 로드
-		if (userList.length > 0) {
-			currentUser = userList[0]; // 첫 번째 사용자만 선택
-			user.set(currentUser); // 스토어에 설정
-		} else {
-			user.set(undefined); // 목록이 비어 있는 경우
-		}
-	});
-
-	// 스토어 구독
-	$: currentUser = $user; // 구독을 통해 스토어의 값을 가져옴
+import { Tabs, TabItem } from "flowbite-svelte";
+import { user } from "../../../stores/user";
+import { isUser } from "$lib/types/User";
 </script>
 
 {#if isUser($user)}
