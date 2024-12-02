f2ce0e491d5183e1e3bc7cf530625df2c913f713
devcomfort <kimdonghyun026@gmail.com>
2024-12-02 03:20:28 +0000

    fix : zip_code -> zipCode 컨벤션 맞춤

--- backend/src/utils/tables/users/addUser.ts
+++ backend/src/utils/tables/users/addUser.ts
@@ -13,7 +13,7 @@ export const addUser = async (user: AddUserArgs) => {
 	// 비밀번호 암호화
 	const { hashed } = generatePasswordWithHash(password);
 
-	return client.users.create({
+	return await client.users.create({
 		data: {
 			// 기존 사용자 데이터
 			id,
@@ -24,10 +24,12 @@ export const addUser = async (user: AddUserArgs) => {
 			addresses: {
 				// TODO: 추후 주소 데이터를 외부에서 입력받을 수 있도록 수정하기.
 				// 현재는 프론트엔드도 상세주소와 우편번호를 받기에는 준비되지 않아서, 시간상 빈 문자열로 처리함.
-				create: {
-					address: address,
-					detailedAddress: "",
-					zip_code: "",
+				createMany: {
+					data: {
+						address: address,
+						detailedAddress: "",
+						zipCode: "",
+					},
 				},
 			},
 			// 생성된 해시
